022f8fc38ecff066fe689ad279c8b85db6153700
Levonhar930 <levonhar93@mail.ru>
2020-02-18 23:49:24 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Menu from './Menu';
+import Content from './Content';
+
+
+function App() {
+  return (
+    <div className='app-wrapper'>
+     <Header/>
+     <Menu/>
+     <Content/>
+
+     
+    </div>
+  );
+}
+
+
+
+export default App;
+
---
+++ src/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Content.module.css'
+const User = () => {
+    return(
+        <div>
+            <img src = 'https://img.faceyourmanga.com/mangatars/1/873/1873552/large_1751979.png' class = {classes.avatar}/>
+
+        </div>
+    )
+}
+export default User;
